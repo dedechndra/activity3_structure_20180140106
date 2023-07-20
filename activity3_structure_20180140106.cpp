9b232275374ae4dc8adec7ee439f94d8ca1a1847
dedechndra <dedech888@gmail.com>
2023-07-21 00:47:55 +0700

    create int main mahasiswa

--- activity3_structure_20180140106.cpp
+++ activity3_structure_20180140106.cpp
@@ -9,3 +9,21 @@ struct Mahasiswa
 	int umur;
 
 };
+int main()
+{
+	Mahasiswa mhs;
+
+	cout << "Masukkan NIM = ";
+	cin >> mhs.NIM;
+	cout << "Masukkan Nama = ";
+	cin >> mhs.Nama;
+	cout << "Masukkan Alamat = ";
+	cin >> mhs.Alamat;
+	cout << "Masukkan Umur = ";
+	cin >> mhs.umur;
+
+	cout << "\nNim = " << mhs.NIM;
+	cout << "\nNama = " << mhs.Nama;
+	cout << "\nAlamat = " << mhs.Alamat;
+	cout << "\nUmur = " << mhs.umur;
+}
